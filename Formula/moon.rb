28801ac23269b0aff3a4396d5054ac52c415573d
Nikolai Kozak <nikokozak@gmail.com>
2021-09-23 14:57:23 -0600

    fix sha

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -2,7 +2,7 @@ class Moon < Formula
   desc "Display a daily, weekly, or monthly moon-phase calendar in your terminal!"
   homepage "https://github.com/nikokozak/moon"
   url "https://github.com/nikokozak/moon/raw/main/dist/0.0.1.tar"
-  sha256 "153f4ba9e11de443f952355654d767a9206f2516d95560e1f970cb8840d2eac2"
+  sha256 "0e60e0621ac9c9888603be06c408733ef351590a5e6e33af4e9c737eb7653bdd"
   license "MIT"
 
   depends_on "ruby" if Hardware::CPU.arm?
