e839cbdbdc343cb2f058df38088a936ecfc02f0d
Nikolai Kozak <nikokozak@gmail.com>
2021-09-23 13:35:35 -0600

    moon formula

---
+++ Formula/moon.rb
@@ -0,0 +1,22 @@
+class Moon < Formula
+  desc "Display a daily, weekly, or monthly moon-phase calendar in your terminal!"
+  homepage "https://github.com/nikokozak/moon"
+  url "https://github.com/nikokozak/moon/dist/moon.tar.gz"
+  license "MIT"
+
+  depends_on "ruby" if Hardware::CPU.arm?
+
+  uses_from_macos "ruby", since: :catalina
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "moon.gemspec"
+    system "gem", "install", "moon-#{version}.gem"
+
+    bin.install libexec/"bin/moon"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Nikokozak Formulas
+
+## How do I install these formulae?
+
+`brew install nikokozak/formulas/<formula>`
+
+Or `brew tap nikokozak/formulas` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
