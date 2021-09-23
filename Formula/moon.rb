251de3be647008f064c75b04252b9f13ae5edfb0
Nikolai Kozak <nikokozak@gmail.com>
2021-09-23 14:46:22 -0600

    fixing compression

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,7 +1,7 @@
 class Moon < Formula
   desc "Display a daily, weekly, or monthly moon-phase calendar in your terminal!"
   homepage "https://github.com/nikokozak/moon"
-  url "https://github.com/nikokozak/moon/blob/main/v0.0.1.tar.gz"
+  url "https://github.com/nikokozak/moon/blob/main/dist/0.0.1.tar"
   sha256 "d16874076ca05c4dff561d0e21312c721e89efebbca5916d9e6d1550cd1785f2"
   license "MIT"
 
