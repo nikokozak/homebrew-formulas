5db60316093a0c202166d7d5f0f4c705ec212336
Nikolai Kozak <nikokozak@gmail.com>
2021-09-23 14:56:19 -0600

    trying

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,7 +1,7 @@
 class Moon < Formula
   desc "Display a daily, weekly, or monthly moon-phase calendar in your terminal!"
   homepage "https://github.com/nikokozak/moon"
-  url "https://github.com/nikokozak/moon/blob/main/dist/0.0.1.tar"
+  url "https://github.com/nikokozak/moon/raw/main/dist/0.0.1.tar"
   sha256 "153f4ba9e11de443f952355654d767a9206f2516d95560e1f970cb8840d2eac2"
   license "MIT"
 
