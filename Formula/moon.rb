767c7e1356d91c3534bbf0351ce85cbc3dcfa694
Nikolai Kozak <nikokozak@gmail.com>
2021-09-23 21:17:16 -0600

    version 0.1.1 of moon

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -1,21 +1,17 @@
 class Moon < Formula
-  desc "Display a daily, weekly, or monthly moon-phase calendar in your terminal!"
+  desc "Display a moon calendar in your terminal!"
   homepage "https://github.com/nikokozak/moon"
-  url "https://github.com/nikokozak/moon/raw/main/dist/0.0.1.tar"
-  sha256 "a5b2cfb7564a3052743e6e92c8efa87fca380d0b445188e33977371c68c21710"
+  url "https://github.com/nikokozak/moon/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "e0007d9e772af22a2d3c01b8a214cebf971d022447f766bb5fe97c945b5e9e60"
   license "MIT"
-
-  depends_on "ruby" if Hardware::CPU.arm?
-
-  uses_from_macos "ruby", since: :catalina
+  
+  # Taken from Cocoapods
+  depends_on "ruby" if Hardware::CPU.arm? 
+  uses_from_macos "ruby", since: :catalina 
 
   def install
-    ENV["GEM_HOME"] = libexec
     system "gem", "build", "moon.gemspec"
-    system "gem", "install", "mooncal-#{version}.gem"
-
-    bin.install libexec/"bin/moon"
-    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    system "gem", "install", "mooncal-*.gem"
   end
 
   test do
